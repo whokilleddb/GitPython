78a17f5c8c0bdad62183dae71f593cacfbfea5fe
whokilleddb <whokilleddb@gmail.com>
2022-06-01 16:41:04 +0530

    Temporary File

--- git/index/base.py
+++ git/index/base.py
@@ -351,7 +351,7 @@ class IndexFile(LazyMixin, git_diff.Diffable, Serializable):
 
         # tmp file created in git home directory to be sure renaming
         # works - /tmp/ dirs could be on another device
-        tmp_index = tempfile.NamedTemporaryFile(dir=repo.git_dir, delete=True).name
+        tmp_index = tempfile.TemporaryFile(dir=repo.git_dir).name
 
         arg_list.append("--index-output=%s" % tmp_index)
         arg_list.extend(treeish)
